2ceafdcb90467957c9141667e392663e6027f6ac
Termininja <termininja@gmail.com>
2015-03-23 13:10:37 +0200

    The project - first commit
    Some basic subprojects, classes, interfaces, etc.

---
+++ FitnessManager/Fitness.Manager/FitnessConsole.cs
@@ -0,0 +1,19 @@
+﻿namespace Fitness.Console
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+    using System.Threading.Tasks;
+
+    using Fitness.Models.Users;
+
+    public class FitnessManager
+    {
+        public static void Main()
+        {
+            User user = null;
+
+        }
+    }
+}
---
+++ FitnessManager/Fitness.Models/Feedings/RookiesNutritions.cs
@@ -0,0 +1,14 @@
+﻿namespace Fitness.Models.Feedings
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+    using System.Threading.Tasks;
+
+    using Fitness.Models.Interfaces;
+
+    class RookiesNutritions : Feeding, IFeedRookies
+    {
+    }
+}

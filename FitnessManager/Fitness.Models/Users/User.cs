ca73e0a4efced60b2ff4efa7b63ee55bd73e1672
AndrewMitev <andreimitev93@abv.bg>
2015-03-23 18:36:29 +0200

    Initial properties for user

--- FitnessManager/Fitness.Models/Users/User.cs
+++ FitnessManager/Fitness.Models/Users/User.cs
@@ -3,11 +3,33 @@
     using System;
     using System.Collections.Generic;
     using System.Linq;
-    using System.Text;
+    using System.Text.RegularExpressions;
     using System.Threading.Tasks;
 
     public abstract class User
     {
+        private string username;
 
+        private string password;
+
+        private string avatarPath;
+
+        public string Username
+        {
+            get
+            {
+                return this.username;
+            }
+            set
+            {
+                if (Regex.IsMatch(value, "[a-zA-Z0-9]"))
+                {
+                    this.username = value;
+                }
+            }
+        }
+
+        public string Password { set; private get; }
+        public string AvatarPath { set; get; }
     }
 }

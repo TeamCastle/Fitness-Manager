fda4b3413d4c8b0bdaa7bd3e32e4872a95a11113
AndrewMitev <andreimitev93@abv.bg>
2015-03-23 17:51:32 +0200

    Revert "Nothing"
    This reverts commit f2ea76b964b4a4f3b6bac8605d034c387fa472f2.

---
+++ FitnessManager/Users/User.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text.RegularExpressions;
+
+namespace Users
+{
+    internal abstract class User
+    {
+        private string username;
+
+        private string password;
+
+        private string avatarPath;
+
+        public string Username
+        {
+            get 
+            {
+                return this.username;
+            }
+
+            set
+            {
+                if (Regex.IsMatch(value, "[a-zA-Z0-9]"))
+                {
+                    this.username = value;
+                }
+            }
+        }
+
+        protected string Password { set; private get;}
+
+        public string AvatarPath { set; get;}
+
+        private User()
+        { 
+        
+        }
+
+
+    }
+}
